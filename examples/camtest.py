855dd79d4efc44c601e743425f0ea169470a40f0
Chris Hedberg <ChristophHedberg@gmail.com>
2019-01-10 14:17:28 -0500

    tweaks

--- examples/camtest.py
+++ examples/camtest.py
@@ -1,6 +1,6 @@
 import face_recognition
 import cv2
-
+import time
 
 video_capture = cv2.VideoCapture('rtsp://10.0.1.43:8554/unicast')
 
@@ -20,7 +20,6 @@ fiona_face_encoding = face_recognition.face_encodings(fiona_image)[0]
 oliver_image = face_recognition.load_image_file("Oliver.jpg")
 oliver_face_encoding = face_recognition.face_encodings(oliver_image)[0]
 
-
 # Create arrays of known face encodings and their names
 known_face_encodings = [
     chris_face_encoding,
@@ -37,31 +36,23 @@ known_face_names = [
     "Oliver Hedberg"
 ]
 
-
 # Initialize some variables
 face_locations = []
 face_encodings = []
 face_names = []
-process_this_frame = True
 
 while True:
     # Grab a single frame of video
     ret, frame = video_capture.read()
 
-    print("ret:" + str(ret))
-    print("frame:" + str(frame))
+    if ret:
 
-    if frame is None:
-        print("video_capture.read() returned ret=false")
+        # Resize frame of video to 1/4 size for faster face recognition processing
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
-    # Resize frame of video to 1/4 size for faster face recognition processing
-    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+        rgb_small_frame = small_frame[:, :, ::-1]
 
-    # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-    rgb_small_frame = small_frame[:, :, ::-1]
-
-    # Only process every other frame of video to save time
-    if process_this_frame:
         # Find all the faces and face encodings in the current frame of video
         face_locations = face_recognition.face_locations(rgb_small_frame)
         face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
@@ -79,29 +70,34 @@ while True:
 
             face_names.append(name)
 
-    process_this_frame = not process_this_frame
 
+        # Display the results
+        for (top, right, bottom, left), name in zip(face_locations, face_names):
+            # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
 
-    # Display the results
-    for (top, right, bottom, left), name in zip(face_locations, face_names):
-        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-        top *= 4
-        right *= 4
-        bottom *= 4
-        left *= 4
+            print("Found This Person: " + name)
 
-        print("Found This Person: " + name)
+            # Draw a box around the face
+            #cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
 
-        # Draw a box around the face
-        #cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+            # Draw a label with a name below the face
+            #cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+            #font = cv2.FONT_HERSHEY_DUPLEX
+            #cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
-        # Draw a label with a name below the face
-        #cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-        #font = cv2.FONT_HERSHEY_DUPLEX
-        #cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+        # Display the resulting image
+        #cv2.imshow('Video', frame)
 
-    # Display the resulting image
-    #cv2.imshow('Video', frame)
+    print("sleep")
+
+    #wait 1/2 of a second
+    time.sleep(0.5)
+
+    print("restart")
 
     # Hit 'q' on the keyboard to quit!
     if cv2.waitKey(1) & 0xFF == ord('q'):
