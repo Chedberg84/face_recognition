588ebe33f8c5220b77ef5bfa1df83b16b46e5a10
Chris Hedberg <ChristophHedberg@gmail.com>
2019-01-10 13:43:27 -0500

    update

--- examples/camtest.py
+++ examples/camtest.py
@@ -48,7 +48,7 @@ while True:
     # Grab a single frame of video
     ret, frame = video_capture.read()
 
-    if !ret:
+    if ret is None:
         print("video_capture.read() returned ret=false")
 
     # Resize frame of video to 1/4 size for faster face recognition processing
