ae74878dc567bab0d08a4dd53391a1a42fee834e
Chris Hedberg <ChristophHedberg@gmail.com>
2019-01-10 22:10:19 -0500

    trying something

--- examples/camtest.py
+++ examples/camtest.py
@@ -47,21 +47,26 @@ while True:
 
     if ret:
         print("ret:true")
+        print("frame:" + str(frame))
 
         # Resize frame of video to 1/4 size for faster face recognition processing
         #small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
-        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-        rgb_small_frame = small_frame[:, :, ::-1]
+        #print("small_frame:" + str(small_frame))
 
-        print("rgb_small_frame:" + str(rgb_small_frame))
+        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+        #rgb_small_frame = small_frame[:, :, ::-1]
+
+        #print("rgb_small_frame:" + str(rgb_small_frame))
 
         # Find all the faces and face encodings in the current frame of video
-        face_locations = face_recognition.face_locations(rgb_small_frame)
+        #face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_locations = face_recognition.face_locations(frame)
 
         print("face_locations:" + str(face_locations))
 
-        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+        #face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+        face_encodings = face_recognition.face_encodings(frame, face_locations)
 
         print("face_encodings:" + str(face_encodings))
 
